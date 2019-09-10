d94d14eb7e942ba47f7501cd828ec730a25089dc
Takatsu Toshiki <takatsu@zereo.co.jp>
2019-09-10 20:08:08 +0900

    configにauthパッケージのロード設定 auth, simpleauth のconfigをカスタムするためコピー

---
+++ auth_app/fuel/app/config/auth.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Auth configurations
+ */
+
+return array(
+    // 'driver'                 => 'Simpleauth',
+    // 'verify_multiple_logins' => false,
+    'salt'                   => 'yr3#Pix{?/(7YI9(0(&YNL9/%_F.+-ej+)#|mPL3dJBv$mt<|}yxoA9>|8&9^skb',
+    // 'iterations'             => 10000,
+);
